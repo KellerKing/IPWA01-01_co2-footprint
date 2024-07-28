70dfbf5d5ded4946c9a3758f65b11a12e200defa
Luca-Keller <Andreasks56@gmail.com>
2024-07-28 08:52:25 +0200

    Filter für Land und Unternehmen implementiert sowie Daten angelegt. In einem zukünftigeren Schritt soll das alles refactored werden. Es soll eine ViewModel an die Tabelle übergeben werden und die Sortier/Filterlogik soll in eine separater Klasse ausgelagert werden.

---
+++ src/lib/tabellendaten.js
@@ -0,0 +1,11 @@
+export const datenTabelle = [
+    { Land: "Deutschland", Unternehmen: "SEW", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "SAP", Co2InGrammProStundeBetrieb: 50, Branche: "Industrie", IsVisible: true },
+    { Land: "Algerien", Unternehmen: "B", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "D", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "a", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "asd", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "ac", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "sdfsdfa", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true },
+    { Land: "Deutschland", Unternehmen: "vsdf", Co2InGrammProStundeBetrieb: 20, Branche: "Industrie", IsVisible: true }
+];
